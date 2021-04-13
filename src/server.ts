9ff47f24dbaea124b0774a983d9834648421a461
eliasall3x <elliasallex@gmail.com>
2021-04-13 16:59:31 -0300

    feat: errors threat

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import "express-async-errors";
+import express, { NextFunction, Response, Request } from "express";
 
+import { AppError } from "./errors/AppError";
 import { routes } from "./routes";
 
 const app = express();
@@ -7,6 +9,21 @@ const app = express();
 // MIDDLEWARES
 app.use(express.json());
 app.use(routes);
+// Errors handle
+app.use(
+  (err: Error, _request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 app.listen(process.env.PORT || 3333, () =>
   console.log(`SERVER IS RUNNING IN PORT = ${3333}`)

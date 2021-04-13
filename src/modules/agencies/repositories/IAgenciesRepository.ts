0b8f843d94ecf1d4bf2faf5a8668f0a9b9dff1ec
eliasall3x <elliasallex@gmail.com>
2021-04-13 15:16:26 -0300

    feat: added agency method description

--- src/modules/agencies/repositories/IAgenciesRepository.ts
+++ src/modules/agencies/repositories/IAgenciesRepository.ts
@@ -9,25 +9,29 @@ export interface IAgencyData {
 export interface IAgenciesRepository {
   /**
    * will create a new agency member
-   * @param create
+   * @param name the name of the agency
+   * @param email the email/indentification of the agency
+   * @param password the secrete word to loggin
    */
   create({ name, email, password }: IAgencyData): void;
 
   /**
    * will list all agencies created
-   * @param list
+   * @return list with all entities/agencies
    */
   list(): Agency[];
 
   /**
    * will return position of the agency in list
-   * @param findAgencyPosition
+   * @param email the email/indentification of the agency
+   * @return id of the agency
    */
   findAgencyPosition(email: string): number;
 
   /**
    * will remove an agency in list
-   * @param delete
+   * @param email the email/indentification of the agency
+   * @return all data of entitie/agency removed
    */
   delete(email: string): Agency[];
 }

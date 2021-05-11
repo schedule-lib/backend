570a64939539e4e7b3b40871abce50c4d9a0a020
eliasall3x <elliasallex@gmail.com>
2021-05-11 16:38:34 -0300

    feat: agency serials

--- src/modules/services/entities/Service.ts
+++ src/modules/services/entities/Service.ts
@@ -9,6 +9,27 @@ class Service {
   @Column()
   name: string;
 
+  @Column()
+  agency_owner: string;
+
+  @Column()
+  fields_to_fill: string;
+
+  @Column()
+  addresses: string;
+
+  @Column()
+  months: string;
+
+  @Column()
+  start_hours: string;
+
+  @Column()
+  end_hours: string;
+
+  @Column()
+  on_weekends: boolean;
+
   @CreateDateColumn()
   created_at: Date;
 

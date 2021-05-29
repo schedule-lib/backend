d691bc24e219b449c3553df2ed923a252f72776e
eliasall3x <elliasallex@gmail.com>
2021-05-29 18:48:35 -0300

    feat: CreateScheduler useCase

---
+++ src/modules/accounts/useCases/createScheduler/CreateSchedulerController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+
+import { CreateSchedulerUseCase } from "./CreateSchedulerUseCase";
+
+class CreateSchedulerController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const createSchedulerUseCase = container.resolve(CreateSchedulerUseCase);
+
+    const {
+      code,
+      hour,
+      phone_number,
+      point,
+      province,
+      service,
+      username,
+    } = request.body;
+
+    await createSchedulerUseCase.execute({
+      code,
+      hour,
+      phone_number,
+      point,
+      province,
+      service,
+      username,
+    });
+
+    return response.status(201).json();
+  }
+}
+export { CreateSchedulerController };
---
+++ src/modules/accounts/useCases/createScheduler/CreateSchedulerUseCase.ts
@@ -0,0 +1,42 @@
+import { inject, injectable } from "tsyringe";
+
+import { IScheduledRepository } from "@modules/accounts/repositories/IScheduledRepository";
+
+interface IRequest {
+  code: string;
+  username: string;
+  service: string;
+  point: string;
+  phone_number: string;
+  province: string;
+  hour: string;
+}
+
+@injectable()
+class CreateSchedulerUseCase {
+  constructor(
+    @inject("ScheduledRepository")
+    private scheduledRepository: IScheduledRepository
+  ) {}
+
+  async execute({
+    code,
+    hour,
+    phone_number,
+    point,
+    province,
+    service,
+    username,
+  }: IRequest): Promise<void> {
+    await this.scheduledRepository.create({
+      code,
+      hour,
+      phone_number,
+      point,
+      province,
+      service,
+      username,
+    });
+  }
+}
+export { CreateSchedulerUseCase };
